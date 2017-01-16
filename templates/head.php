ef541276db3774d3a1a573482748f32affac5126
shimoyama-tomoaki <gd27.d.shimoyama@gmail.com>
2017-01-16 11:42:32 +0900

    [fix] パス指定を絶対パスに変更

--- templates/head.php
+++ templates/head.php
@@ -1,3 +1,3 @@
-    <link rel="stylesheet" href="./common/css/style.css">
-    <link rel="stylesheet" href="./common/css/normalize.css">
+    <link rel="stylesheet" href="/common/css/style.css">
+    <link rel="stylesheet" href="/common/css/normalize.css">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300" rel="stylesheet">
