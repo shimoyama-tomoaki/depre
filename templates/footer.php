d318bdcc057f6e6b991eec1fb880c8b36125871b
shimoyama-tomoaki <gd27.d.shimoyama@gmail.com>
2017-01-16 11:18:58 +0900

    [fix] phpファイル化

---
+++ templates/footer.php
@@ -0,0 +1,18 @@
+
+  <footer>
+    <div class="footer__wrapper clearfix">
+      <ul class="footer__list">
+        <li class="footer__item"><a href="#">サイトのご利用について</a></li>
+        <li class="footer__item"><a href="#">サイトマップ</a></li>
+        <li class="footer__item"><a href="#">個人情報保護方針</a></li>
+        <li class="footer__item"><a href="#">特定個人情報基本方針</a></li>
+        <li class="footer__item"><a href="#">ソーシャルメディアポリシー</a></li>
+      </ul>
+      <h2 class="footer__logo">
+        <a href="#">
+          <img src="images/footer_logo.png" width="138" height="46" alt="">
+        </a>
+      </h2>
+    </div>
+    <p class="footer__copy">:Depro　Copyright :Depro CO.,LTD All rights reserved.</p>
+  </footer>
---
+++ templates/header.php
@@ -0,0 +1,37 @@
+
+  <header>
+    <div class="header__inner">
+      <h1 class="header__logo"><a href="#"><img src="images/logo.png" width="100" height="33" alt=""></a></h1>
+      <div class="header__share share">
+        <ul class="share__list">
+          <li class="share__item"><a href="#"><img src="common/images/header/twitter_icon.jpg" width="23" height="23" alt=""></a></li>
+          <li class="share__item"><a href="#"><img src="common/images/header/facebook_icon.jpg" width="23" height="23" alt=""></a></li>
+          <li class="share__item"><a href="#"><img src="common/images/header/line_icon.jpg" width="23" height="23" alt=""></a></li>
+        </ul>
+      </div>
+
+      <div class="header--sm">
+        <a href="#">
+          <img src="images/menu.png" width="20" height="auto" alt="">
+        </a>
+      </div>
+    </div>
+    <div class="header--sm__menu">
+      <div class="nav nav--sm">
+        <ul class="nav__list nav__list--sm">
+          <li class="nav__item"><a href="#"><img src="images/what’s_nav.png" width="212" height="24" alt=""></a></li>
+          <li class="nav__item"><a href="#"><img src="images/Improve_nav.png" width="240" height="25" alt=""></a></li>
+          <li class="nav__item"><a href="#"><img src="images/Column_nav.png" width="79" height="19" alt=""></a></li>
+          <li class="nav__item"><a href="#"><img src="images/Twitter_nav.png" width="65" height="18" alt=""></a></li>
+          <li class="nav__item"><a href="#"><img src="images/Contact_nav.png" width="79" height="19" alt=""></a></li>
+        </ul>
+      </div>
+      <div class="header__share share share--sm">
+        <ul class="share__list clearfix">
+          <li class="share__item"><a href="#"><img src="common/images/header/twitter_icon.jpg" width="23" height="23" alt=""></a></li>
+          <li class="share__item"><a href="#"><img src="common/images/header/facebook_icon.jpg" width="23" height="23" alt=""></a></li>
+          <li class="share__item"><a href="#"><img src="common/images/header/line_icon.jpg" width="23" height="23" alt=""></a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
